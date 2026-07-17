e3ec2d9ebc6ac8538e24199e005b18449ee12f22
dnyaneshwaribonde <dnyaneshwaribonde55@gmail.com>
2026-07-17 15:39:56 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,23 @@
+// Smooth Scrolling
+
+const links = document.querySelectorAll('nav a');
+
+links.forEach(link => {
+
+    link.addEventListener('click', function(e){
+
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+
+        if(target){
+
+            target.scrollIntoView({
+                behavior:'smooth'
+            });
+
+        }
+
+    });
+
+});
